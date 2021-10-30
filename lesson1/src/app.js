8594af5bec28473a5d9d263daac1736a8fe6cd5e
Arkadiy Andriyenko <andriyenko@lohika.com>
2021-10-30 17:10:24 +0300

    Added .editorconfig file and formatted existing code base

--- lesson1/src/app.js
+++ lesson1/src/app.js
@@ -1,13 +1,17 @@
 const http = require('http');
 
 function StartServer(port, env, logger) {
-  http.createServer((req,res) => {
-  logger.log('New request');
-  res.writeHeader(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({message: 'Hello world!'}));
-}).listen(port, () => {logger.log(`Server is listening on port: ${port} . Env is ${env}`)});
+  http
+    .createServer((req, res) => {
+      logger.log('New request');
+      res.writeHeader(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Hello world!' }));
+    })
+    .listen(port, () => {
+      logger.log(`Server is listening on port: ${port} . Env is ${env}`);
+    });
 }
 
 module.exports = {
-  StartServer
-}
+  StartServer,
+};
--- lesson1/src/config.js
+++ lesson1/src/config.js
@@ -2,13 +2,14 @@ const APP_PORT = process.env.APP_PORT || 3030;
 const args = process.argv.slice(2);
 let ENV;
 
-for(let i = 0; i < args.length; i++) {
-  if(args[i].slice(2).toLowerCase() === 'env') {
+for (let i = 0; i < args.length; i++) {
+  if (args[i].slice(2).toLowerCase() === 'env') {
     ENV = args[i + 1];
     break;
   }
 }
 
 module.exports = {
-  ENV, APP_PORT
-}
+  ENV,
+  APP_PORT,
+};
